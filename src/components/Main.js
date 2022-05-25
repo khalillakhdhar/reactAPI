9fb02b5de2c65159c096939e99dee04c1b0c3d24
khalil <khalillakhdharatc@gmail.com>
2022-05-25 16:15:39 +0100

    Revert "Revert "pour le prof""
    This reverts commit 56ea2171b8a086c7156311b11ddf13ef5d1c8d31.

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+class Main extends React.Component {
+render() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          
+        </div>
+        <div className="col">
+        <h1>Bienvenue</h1>
+        </div>
+        <div className="col">
+          
+        </div>
+      </div>
+    </div>
+      
+  );
+  }
+  }
+  export default Main;
---
+++ src/components/layout/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+  Link
+  } from "react-router-dom";
+class Nav extends React.Component {
+render() {
+return (
+<>
+            
+    <hr />
+            </>
+
+
+
+    )
+}
+}
+export default Nav
