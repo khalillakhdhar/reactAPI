7e66cbcfae48f0c48da51c8523c5a25499bedf22
khalil <khalillakhdharatc@gmail.com>
2022-05-25 16:24:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React from 'react'
-import './App.css'
-import Nav from './components/layout/Nav';
+import React, { Component } from 'react';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import './App.css';
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-import CreateStudent from './components/create-student.component'
-import EditStudent from './components/edit-student.component'
-import StudentList from './components/list-student.component'
 import Main from './components/Main';
-
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return (
       <Router>
-        <Nav/>
-
-                <Switch>
-                <Route path="/" element={<Main/>}>
-                  </Route>
-                  
-                </Switch>
+        
+        <div>
+          <switch>
+          <Route exact path='/' component={Main} />
+          
+          </switch>
+        </div>
       </Router>
-    </div>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
